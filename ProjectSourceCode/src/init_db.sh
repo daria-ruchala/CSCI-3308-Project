ef4902754f4565c86f656fed482c292c7be8d55b
Emilianomedina10 <123131033+Emilianomedina10@users.noreply.github.com>
2025-04-17 18:14:45 -0600

    Update init_db.sh

--- ProjectSourceCode/src/init_db.sh
+++ ProjectSourceCode/src/init_db.sh
@@ -4,7 +4,7 @@
 # This file contains sensitive information and should be kept private
 
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="postgresql://group8:kOeAdRjJQeuj0LvaS86hgNWN6sMCynKw@dpg-d00oq2buibrs73en6utg-a.oregon-postgres.render.com/users_db_ybrh"
+PG_URI=""
 
 # Execute each .sql file in the directory
 for file in init_data/*.sql; do
